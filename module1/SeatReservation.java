0114c6997ca93c77a359766078bcec9459da7cec
John A. Osho <ajetunmobiosh@gmail.com>
2026-01-10 08:30:43 -0600

    Added hamcrest-core-1.3.jar, junit-4.13.2.jar and SeatReservationTest.java to the module1 directory.  Made a small change to setFlightDesignator()

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -12,7 +12,10 @@ public class SeatReservation{
    }
 
    public void setFlightDesignator(String fd) {
-      this.flightDesignator = fd;
+      if (fd == null){
+         throw new IllegalArgumentException("flight designator cannot be null");
+      }
+         this.flightDesignator = fd;
    }
 
    public java.time.LocalDate getFlightDate() {
