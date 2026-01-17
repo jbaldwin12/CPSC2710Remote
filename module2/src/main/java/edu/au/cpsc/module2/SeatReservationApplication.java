c4f2a0d8c04ac831faf0cc8c667164229100e249
Jordan Baldwin <jtb0185@auburn.edu>
2026-01-17 16:13:15 -0600

    all controls have been added

--- module2/src/main/java/edu/au/cpsc/module2/SeatReservationApplication.java
+++ module2/src/main/java/edu/au/cpsc/module2/SeatReservationApplication.java
@@ -3,12 +3,10 @@ package edu.au.cpsc.module2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.time.LocalDate;
 
@@ -17,17 +15,32 @@ public class SeatReservationApplication extends Application {
     public void start(Stage stage) throws IOException {
         CheckBox flyingWithInfantCB = getFlyingWithInfantCB();
         CheckBox flyingWithTravelInsuranceCB = getFlyingWithTravelInsuranceCB();
-        TextField flightDesignatorTF = new TextField("Flight Designator: ");
-        TextField firstNameTF = new TextField("First Name: ");
-        TextField lastNameTF = new TextField("Last Name: ");
+        Label flightDesignatorLabel = new Label("Flight Designator:");
+        TextField flightDesignatorTF = new TextField();
+        Label firstNameLabel = new Label("First Name:");
+        TextField firstNameTF = new TextField();
+        Label lastNameLabel = new Label("Last Name:");
+        TextField lastNameTF = new TextField();
         DatePicker flightDate = getFlightDate();
-        VBox root = new VBox(flyingWithInfantCB, flyingWithTravelInsuranceCB, flightDesignatorTF,
-                firstNameTF, lastNameTF, flightDate);
-        Scene scene = new Scene(root);
+        Label numberOfPassengersLabel = new Label("Number of Passengers:");
+        TextField numberOfPassengers = new TextField("1");
+        numberOfPassengers.setEditable(false);
+        GridPane buttonPane = getButtonPane();
+        VBox root = new VBox(flyingWithInfantCB, flyingWithTravelInsuranceCB, flightDesignatorLabel, flightDesignatorTF,
+                firstNameLabel, firstNameTF, lastNameLabel, lastNameTF, flightDate, numberOfPassengersLabel,
+                numberOfPassengers, buttonPane);
+        Scene scene = new Scene(root, 500, 300);
         stage.setTitle("Jordan Baldwin's Seat Reservation App");
         stage.setScene(scene);
         stage.show();
 
+    }
+
+    private static GridPane getButtonPane() {
+        GridPane buttonPane = new GridPane();
+        buttonPane.add(new Button("cancel"), 0, 7);
+        buttonPane.add(new Button("save"), 1, 7);
+        return buttonPane;
     }
 
     private static DatePicker getFlightDate() {
