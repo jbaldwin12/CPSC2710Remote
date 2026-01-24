36d8c64b6c014e864988ca63d1a2d9ec3d4f2280
Jordan Baldwin <jtb0185@auburn.edu>
2026-01-23 22:26:35 -0600

    created airport.java file

---
+++ module3/src/main/java/edu/au/cpsc/module3/Airport.java
@@ -0,0 +1,12 @@
+/*
+* Project Name: Airport Info. UI
+* Author: Jordan Baldwin
+* Auburn email: jtb0185@auburn.edu
+* Date: 1/23/26
+* Description: User will enter an ID for an airport to display information about it, including a simple map.
+ */
+
+package edu.au.cpsc.module3;
+
+public class Airport {
+}
