7de7550b93a6336d4a2214fac416d4eee8dbd3d1
Jordan Baldwin <jtb0185@auburn.edu>
2026-01-24 18:11:10 -0600

    Create UI window and connected search fields and button

--- module3/src/main/java/edu/au/cpsc/module3/AirportApplication.java
+++ module3/src/main/java/edu/au/cpsc/module3/AirportApplication.java
@@ -11,20 +11,23 @@ import java.util.List;
 import static edu.au.cpsc.module3.Airport.printAirports;
 
 public class AirportApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
+
         // Read Airport list from file
         try {
             List<Airport> airportList = Airport.readAll();
             System.out.printf("Records = %-10d%n", airportList.size());
-            //printAirports(airportList);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        // Load Scene Builder FXML and Initiate UI Window
         FXMLLoader fxmlLoader = new FXMLLoader(AirportApplication.class.getResource("airport-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
+        stage.setMinWidth(800);
+        stage.setMinHeight(600);
         stage.setScene(scene);
         stage.show();
     }
--- module3/src/main/java/edu/au/cpsc/module3/AirportController.java
+++ module3/src/main/java/edu/au/cpsc/module3/AirportController.java
@@ -1,14 +1,41 @@
 package edu.au.cpsc.module3;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class AirportController {
     @FXML
-    private Label welcomeText;
+    private TextField identField;
+    @FXML
+    private TextField iataField;
+    @FXML
+    private TextField localCodeField;
+    @FXML
+    private TextField typeField;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField elevationField;
+    @FXML
+    private TextField countryField;
+    @FXML
+    private TextField regionField;
+    @FXML
+    private TextField municipalityField;
+    @FXML
+    private Button searchButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void handleSearchButton() {
+        System.out.println("Search button clicked!");
+        // Your search logic will go here later
     }
+
+    @FXML
+    private void handleSearchText() {
+        System.out.println("Search text entered!");
+        // Your search logic will go here later
+    }
+
 }
