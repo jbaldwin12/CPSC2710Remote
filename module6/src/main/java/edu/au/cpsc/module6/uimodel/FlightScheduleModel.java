bd9f90a57d18c16cab6e2962b8f7a90da1824696
Jordan Baldwin <jtb0185@auburn.edu>
2026-02-13 14:56:03 -0600

    added properties to FlightSchedulemodel.java

--- module6/src/main/java/edu/au/cpsc/module6/uimodel/FlightScheduleModel.java
+++ module6/src/main/java/edu/au/cpsc/module6/uimodel/FlightScheduleModel.java
@@ -1,4 +1,31 @@
+/*Project name: Flight Schedule Application V2
+ * Author:Jordan Baldwin
+ * auburn email: jtb0185@auburn.edu
+ * Date: 2/13/26
+ * Description: Complete application for editing airline flight information,
+ * including a file-based database so that its data is persistent between runs.
+ */
+
 package edu.au.cpsc.module6.uimodel;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.StringProperty;
+
 public class FlightScheduleModel {
+
+    private final StringProperty flightDesignatorProperty;
+    private final StringProperty departureAirportProperty;
+    private final StringProperty arrivalAirportProperty;
+    private final StringProperty departureTimeProperty;
+    private final StringProperty arrivalTimeProperty;
+    private final BooleanProperty sundayProperty;
+    private final BooleanProperty mondayProperty;
+    private final BooleanProperty tuesdayProperty;
+    private final BooleanProperty wednesdayProperty;
+    private final BooleanProperty thursdayProperty;
+    private final BooleanProperty fridayProperty;
+    private final BooleanProperty saturdayProperty;
+    private final BooleanProperty modifiedProperty;
+    private final BooleanProperty newProperty;
+
 }
