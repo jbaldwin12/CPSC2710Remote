64c43faef3107c4e2dd74bcfb0ac3c62f5044d0f
Jordan Baldwin <jtb0185@auburn.edu>
2026-02-13 15:55:23 -0600

    added FlightScheduleModel method with listeners

--- module6/src/main/java/edu/au/cpsc/module6/uimodel/FlightScheduleModel.java
+++ module6/src/main/java/edu/au/cpsc/module6/uimodel/FlightScheduleModel.java
@@ -9,6 +9,8 @@
 package edu.au.cpsc.module6.uimodel;
 
 import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class FlightScheduleModel {
@@ -28,4 +30,37 @@ public class FlightScheduleModel {
     private final BooleanProperty modifiedProperty;
     private final BooleanProperty newProperty;
 
+    public FlightScheduleModel() {
+        flightDesignatorProperty = new SimpleStringProperty();
+        departureAirportProperty = new SimpleStringProperty();
+        arrivalAirportProperty = new SimpleStringProperty();
+        departureTimeProperty = new SimpleStringProperty();
+        arrivalTimeProperty = new SimpleStringProperty();
+        sundayProperty = new SimpleBooleanProperty();
+        mondayProperty = new SimpleBooleanProperty();
+        tuesdayProperty = new SimpleBooleanProperty();
+        wednesdayProperty = new SimpleBooleanProperty();
+        thursdayProperty = new SimpleBooleanProperty();
+        fridayProperty = new SimpleBooleanProperty();
+        saturdayProperty = new SimpleBooleanProperty();
+        modifiedProperty = new SimpleBooleanProperty();
+        newProperty = new SimpleBooleanProperty();
+
+        flightDesignatorProperty.addListener((observable, oldValue, newValue) -> setModified(true));
+        departureAirportProperty.addListener((observable, oldValue, newValue) -> setModified(true));
+        arrivalAirportProperty.addListener((observable, oldValue, newValue) -> setModified(true));
+        departureTimeProperty.addListener((observable, oldValue, newValue) -> setModified(true));
+        arrivalTimeProperty.addListener((observable, oldValue, newValue) -> setModified(true));
+        sundayProperty.addListener((observable, oldValue, newValue) -> setModified(true));
+        mondayProperty.addListener((observable, oldValue, newValue) -> setModified(true));
+        tuesdayProperty.addListener((observable, oldValue, newValue) -> setModified(true));
+        wednesdayProperty.addListener((observable, oldValue, newValue) -> setModified(true));
+        thursdayProperty.addListener((observable, oldValue, newValue) -> setModified(true));
+        fridayProperty.addListener((observable, oldValue, newValue) -> setModified(true));
+        saturdayProperty.addListener((observable, oldValue, newValue) -> setModified(true));
+
+
+    }
+
+    public void setModified(boolean m) { modifiedProperty.set(m); }
 }
