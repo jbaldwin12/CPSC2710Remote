5c5452c974b07edf7d1ad6a1b496df3888542907
Jordan Baldwin <jtb0185@auburn.edu>
2026-02-14 23:30:11 -0600

    update NutritionTrackerApplication and added css file

--- module7/src/main/java/edu/au/cpsc/module7/NutritionTrackerApplication.java
+++ module7/src/main/java/edu/au/cpsc/module7/NutritionTrackerApplication.java
@@ -2,18 +2,28 @@ package edu.au.cpsc.module7;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class NutritionTrackerApplication extends Application {
+
+    private static Stage primaryStage;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(NutritionTrackerApplication.class.getResource("MainWindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        stage.setTitle("Nutrition Tracker");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static Stage getPrimaryStage() {
+        return primaryStage;
     }
 }
